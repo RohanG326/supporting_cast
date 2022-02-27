ca9882b7c269cb022fc6b26f537daad8d0580c3d
briantang <brian.g.tang.135@gmail.com>
2022-02-27 12:28:10 -0800

    game Check Status function

--- createtask/templates/hangman.js
+++ createtask/templates/hangman.js
@@ -78,22 +78,37 @@ function guess (letter) {
         wrongGuess=wrongGuess+1;
     }
     //add letter to checkedLetters
-
+    checkedLetters.push(letter);
     //check the game status
-
+    checkGameStatus();
     //lastly, update the display
-
+    displayWord();
 }
 
 function checkGameStatus() {
     //if wrongGuess is 6 or higher, put "You lose" in gameResultText, display the selection div, hide the game div, and return
-
+    if (wrongGuess>=6){
+        document.getElementById("gameResultText").innerText = "Game Over";
+        document.getElementById("game").style.display="none";
+        document.getElementById("selection").style.display="block";
+        return;
+    }
     //check whether every index of letterCheckArray is true
     //if a single one is false, set indexCheck to false and break the loop
     let indexCheck = true;
-
+    for (let i=0, i<letterCheckArray.length; i++){
+        if (letterCheckArray[i]==false){
+            indexCheck=false;
+            break;
+        }
+    }
     //if every index of letterCheckArray is true, put "You win" in gameResultText, display the selection div, hide the game div, and return
-
+    if (indexCheck==true){
+        document.getElementById("gameResultText").innerText = "You Win";
+        document.getElementById("game").style.display="none";
+        document.getElementById("selection").style.display="block";
+        return;
+    }
 }
 
 function displayWord () {
